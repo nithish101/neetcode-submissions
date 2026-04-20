2b1745aa41aff932dbe7aa8240ef626f6c9d8366
Nithish Sabapathy <71940999+nithish101@users.noreply.github.com>
2026-04-19 21:10:36 -0400

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        sol = []
+        stack = []
+
+        def helper(o, c):
+            if len(stack) == 2 * n:
+                if o == c:
+                    sol.append("".join(stack))
+                return
+            if o > c:
+                stack.append(')')
+                helper(o, c + 1)
+                stack.pop()
+            if o < n:
+                stack.append('(')
+                helper(o + 1, c)
+                stack.pop()
+
+        helper(0, 0)
+        return sol
